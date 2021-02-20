8eda6fc52b9949f3e91ae4f0ece6fc7c994277c8
mikekyle <mikekyle@hotmail.co.uk>
2021-02-20 20:01:57 +0000

    With some testing python

---
+++ testpython.py
@@ -0,0 +1,15 @@
+import json
+
+
+x = 15
+y = "Just a string"
+z = [1,3,6,9]
+
+print(json.dumps([x,y,z]))
+
+testdict = { "a":1, "b":3, "c":2}
+
+s = json.dumps(testdict, indent=4)
+print(s)
+d = json.loads(s)
+d["c"]
